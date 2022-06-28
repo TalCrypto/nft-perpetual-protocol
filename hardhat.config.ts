a00a978fc7a0263ec6fd6a3a6f685e05ad2c388e
riven-tribe3 <riven@tribe3.xyz>
2022-06-28 09:42:44 +0800

    Added hardhat.config.ts sample

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import { task } from "hardhat/config";
 
-// This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -18,9 +18,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.9",
-  gasReporter: {
-    currency: 'USD',
-    gasPrice: 33
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: { enabled: true, runs: 999999 },
+    },
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+
   },
 };
