f882b856ddfa5fe3857667db0e02094911fa6923
TalCrypto <wnick9381@gmail.com>
2022-11-28 14:01:21 +0800

    [config] modify liquidationFeeRatio as half of mm ratio

--- publish/DeployConfig.ts
+++ publish/DeployConfig.ts
@@ -214,7 +214,7 @@ export class DeployConfig {
   readonly initMarginRequirement = utils.parseEther("0.2"); // 20% - 5x
   readonly maintenanceMarginRequirement = utils.parseEther("0.1"); // 10% - 10x
   readonly partialLiquidationRatio = utils.parseEther("0.125");
-  readonly liquidationFeeRatio = utils.parseEther("0.02"); // 2% - 1/5 of maintenance margin
+  readonly liquidationFeeRatio = utils.parseEther("0.05"); // 2% - 1/2 of maintenance margin
 
   // amm
   readonly legacyAmmConfigMap: Record<string, AmmConfig> = {
