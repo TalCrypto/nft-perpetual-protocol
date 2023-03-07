a99b3d2df3c8e0f188b0893bb767ba95e4b4c28d
TalCrypto <wnick9381@gmail.com>
2023-03-07 17:27:51 +0800

    Revert "fix: add setting bound of mm ratio, removed deprecated centralized functions of insurance fund"
    This reverts commit 2c4d1cd4f6217d657052ebab311339c8dfd7dbd4.

--- contracts/ClearingHouse.sol
+++ contracts/ClearingHouse.sol
@@ -258,14 +258,13 @@ contract ClearingHouse is IClearingHouse, OwnerPausableUpgradeSafe, ReentrancyGu
     }
 
     /**
-     * @notice set maintenance margin ratio, should be smaller than initMarginRatio
+     * @notice set maintenance margin ratio
      * @dev only owner can call
      * @param _maintenanceMarginRatio new maintenance margin ratio in 18 digits
      */
     function setMaintenanceMarginRatio(uint256 _maintenanceMarginRatio) external onlyOwner {
         _requireNonZeroInput(_maintenanceMarginRatio);
         _requireRatio(_maintenanceMarginRatio);
-        require(_maintenanceMarginRatio < initMarginRatio, "CH_WMMR"); // wrong maintenance margin ratio
         maintenanceMarginRatio = _maintenanceMarginRatio;
     }
 
