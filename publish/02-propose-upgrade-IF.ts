ff93f4538107101b973d26d5301e89dae23ce248
TalCrypto <wnick9381@gmail.com>
2023-07-27 13:31:44 +0800

    chore: add upgrade IF propose script

---
+++ publish/02-propose-upgrade-IF.ts
@@ -0,0 +1,26 @@
+import { ethers, network, upgrades, defender } from "hardhat";
+import { ContractAddresses, getAddresses } from "./addresses";
+import { LedgerSigner } from "@ethersproject/hardware-wallets";
+import { AmmInstanceName } from "./Constants";
+import { ClearingHouse__factory, Amm__factory, InsuranceFund__factory } from "../typechain-types";
+import { Signer } from "ethers";
+
+async function main() {
+  // const accounts = await ethers.getSigners();
+  // const ledger = accounts[0];
+
+  const ledger = await new LedgerSigner(ethers.provider, "hid", "m/44'/60'/0'/0");
+  console.log("deployer: ", await ledger.getAddress());
+
+  const addresses = getAddresses(network.name);
+  console.log("Preparing proposal to upgrade IF...");
+  const proposalCH = await defender.proposeUpgrade(addresses.insuranceFund, new InsuranceFund__factory(ledger));
+  console.log("IF upgrade proposal created at:", proposalCH.url);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
