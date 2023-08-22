b425cb01ea36a33b9780418fd2c8a72265953d26
TalCrypto <wnick9381@gmail.com>
2023-08-22 10:26:18 +0800

    chore: add upgrade script for IF and staking pool

--- publish/02-propose-upgrade-IF-SP.ts
+++ publish/02-propose-upgrade-IF-SP.ts
@@ -2,17 +2,21 @@ import { ethers, network, upgrades, defender } from "hardhat";
 import { ContractAddresses, getAddresses } from "./addresses";
 import { LedgerSigner } from "@ethersproject/hardware-wallets";
 import { AmmInstanceName } from "./Constants";
-import { ClearingHouse__factory, Amm__factory, InsuranceFund__factory } from "../typechain-types";
+import { ClearingHouse__factory, Amm__factory, InsuranceFund__factory, ETHStakingPool__factory } from "../typechain-types";
 import { Signer } from "ethers";
 
 async function main() {
-  // const accounts = await ethers.getSigners();
-  // const ledger = accounts[0];
+  const accounts = await ethers.getSigners();
+  const ledger = accounts[0];
 
-  const ledger = await new LedgerSigner(ethers.provider, "hid", "m/44'/60'/0'/0");
-  console.log("deployer: ", await ledger.getAddress());
+  // const ledger = await new LedgerSigner(ethers.provider, "hid", "m/44'/60'/0'/0");
+  // console.log("deployer: ", await ledger.getAddress());
 
   const addresses = getAddresses(network.name);
+  console.log("Preparing proposal to upgrade StakingPool...");
+  const proposalSP = await defender.proposeUpgrade(addresses.ethStakingPool, new ETHStakingPool__factory(ledger));
+  console.log("StakingPool upgrade proposal created at:", proposalSP.url);
+
   console.log("Preparing proposal to upgrade IF...");
   const proposalCH = await defender.proposeUpgrade(addresses.insuranceFund, new InsuranceFund__factory(ledger));
   console.log("IF upgrade proposal created at:", proposalCH.url);
